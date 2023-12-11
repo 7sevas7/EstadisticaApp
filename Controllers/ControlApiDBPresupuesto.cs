93ccea13b52ef19fab23bc7f40133cd7243aa7c0
Sebastian Posadas <s3v4s61@gmail.com>
2023-12-11 14:29:45 -0600

    Nuevo control del api y dbContext

---
+++ Controllers/ControlApiDBPresupuesto.cs
@@ -0,0 +1,51 @@
+﻿using EstadisticaApp.DataAcces.Implement;
+using EstadisticaApp.Models;
+using EstadisticaApp.Utilities;
+using System.Diagnostics;
+
+
+namespace EstadisticaApp.Controllers
+{
+    public class ControlApiDBPresupuesto
+    {
+        //Api
+        private ApiRes apiRes;
+        //PresupuestoMain
+        PresupuestoMain presupuestoMain;
+        public ControlApiDBPresupuesto()
+        {
+            apiRes = new ApiRes();
+            presupuestoMain = new PresupuestoMain();
+
+
+        }
+        public async Task verificarCount() {
+
+            var count = presupuestoMain.BoolCount();
+
+            var presupuestoApi = await GetPresupuestoApi();
+            if (!count) {
+                Debug.Write("Guardandoww============>>>>>>>>");
+                try
+                {
+                    Debug.Write("Guardando============>>>>>>>>");
+                    await presupuestoMain.InsertPresupuestos(presupuestoApi);                    
+                    await Task.CompletedTask;
+                }
+                catch (Exception ex)
+                {
+
+                    throw new Exception(ex.ToString());
+                }
+              
+            }
+          
+          
+
+
+        }
+        public async Task<List<UnidadesPresupuesto>?> GetPresupuestoApi()=> await apiRes.GetsListPresupuesto();
+
+    }
+
+}
